ba603a004a4dd599e18c06dcb80f507406018731
AnkitParte <ankitparte80@gmail.com>
2024-06-16 14:11:04 +0530

    bubble sort revised

--- dsa-practice/June15_24.js
+++ dsa-practice/June15_24.js
@@ -2,7 +2,7 @@
 function selectionSort(arr, n) {
   //code here
   for (let i = 0; i < n; i++) {
-    let tmp=i;
+    let tmp = i;
     for (let j = i + 1; j < n; j++) {
       if (arr[j] < arr[tmp]) tmp = j;
     }
@@ -16,4 +16,19 @@ function selectionSort(arr, n) {
 let arr = [4, 1, 3, 9, 7];
 let n = 5;
 
-selectionSort(arr, n);
+// selectionSort(arr, n);
+
+function bubbleSort(arr, n) {
+  //code here\
+  for (let i = 0; i < n; i++) {
+    for (let j = i + 1; j < n; j++) {
+      if (arr[j] < arr[i]) {
+        [arr[i], arr[j]] = [arr[j], arr[i]];
+      }
+    }
+  }
+  console.log(arr);
+  return arr;
+}
+
+bubbleSort(arr,n)
