33531f38b1b0ecf29313f7548da2a394abe9b405
AnkitParte <ankitparte80@gmail.com>
2024-06-17 16:52:49 +0530

    merge sort added

--- dsa-practice/June17_24.js
+++ dsa-practice/June17_24.js
@@ -2,17 +2,64 @@
 
 function insertionSort(arr, n) {
   //code here
-  for (let i=0;i<n;i++) {
-    let j=i
-    while(j>0 && arr[j] < arr[j-1]) {
-        let temp = arr[j]
-        arr[j] = arr[j-1]
-        arr[j-1] = temp
-        j--
+  for (let i = 0; i < n; i++) {
+    let j = i;
+    while (j > 0 && arr[j] < arr[j - 1]) {
+      let temp = arr[j];
+      arr[j] = arr[j - 1];
+      arr[j - 1] = temp;
+      j--;
     }
-}
-console.log(arr)
-return arr
+  }
+  console.log(arr);
+  return arr;
 }
 
-insertionSort([4,1,3,9,7], 5)
+let arr = [4, 1, 3, 9, 7];
+let n = 5;
+// insertionSort(arr, n)
+
+//!Merge sort - Divide and merge
+
+function mergeSort(arr, low, high) {
+  if (low >= high) return;
+  let mid = Math.floor((low + high) / 2);
+
+  mergeSort(arr, low, mid);
+  mergeSort(arr, mid + 1, high);
+
+  return mergeAll(arr, low, mid, high);
+}
+
+function mergeAll(arr, low, mid, high) {
+  let i = low,
+    j = mid + 1;
+  let newArr = [];
+  while (i <= mid && j <= high) {
+    if (arr[i] <= arr[j]) {
+      newArr.push(arr[i]);
+      i++;
+    } else {
+      newArr.push(arr[j]);
+      j++;
+    }
+  }
+  while (j <= high) {
+    newArr.push(arr[j]);
+    j++;
+  }
+  while (i <= mid) {
+    newArr.push(arr[i]);
+    i++;
+  }
+  let x = 0;
+  for (let k = low; k <= high; k++) {
+    arr[k] = newArr[x];
+    x++;
+  }
+//   console.log("arr", arr);
+  return arr;
+}
+
+let ans = mergeSort(arr, 0, n - 1);
+console.log(ans);
